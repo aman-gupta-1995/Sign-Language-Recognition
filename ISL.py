f2279a54ba0c2d691eabdfbfad08b448d90f7349
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-07-19 22:33:18 +0530

    Update ISL.py

--- ISL.py
+++ ISL.py
@@ -21,6 +21,6 @@ img_dilation = cv2.dilate(cap, kernel, iterations=1)
 cv2.imshow('Input', img)
 cv2.imshow('Erosion', img_erosion)
 cv2.imshow('Dilation', img_dilation)
-
+ 
 cv2.waitKey(0)
 cv2.destroyAllWindows()  
