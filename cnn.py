31a8a9793ccd96c5ef0793ce68456856f7793d37
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-07-19 22:30:52 +0530

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -2,7 +2,7 @@
 
 # Installing Theano 
 # pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
- 
+  
 # Installing Tensorflow
 # Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
 
