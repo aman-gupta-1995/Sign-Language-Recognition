d2d2c17cb82c37d03b8130cf7fa69139ce4ee8e9
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-07-19 22:31:58 +0530

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -61,7 +61,7 @@ test_set = test_datagen.flow_from_directory('test_dataset/test_set',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
-
+ 
 classifier.fit_generator(training_set, 
                          samples_per_epoch = 8000,
                          nb_epoch = 25,
